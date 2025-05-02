7617d39c754497c7af91c62ac19d22199bc1113d
mohilio324 <mehdicherbi944@gmail.com>
2025-05-02 11:15:12 +0100

    Update libr_todo.c

--- libr_todo.c
+++ libr_todo.c
@@ -4,14 +4,22 @@
 #include <windows.h>
 #include <time.h>
 
+
+
+
+void set_color(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+
+
+
 #define MAX_TASK (10)
 
 typedef char string[23];
 
-typedef struct date
-{
-    int year, month, day;
-} date;
+
 
 typedef struct TASK
 {
@@ -30,12 +38,13 @@ void PRINT_MENU()
 
     // code later the menu needed
     system("cls");
-
+set_color(10);
     printf("\n==================================\n");
     printf("\n         MANAGEMNT MENU \n");
     printf("\n==================================\n");
 
     printf("\n1-Display the list of tasks.\n2-Add new tasks.\n3-Mark tasks as completed or remove them.\n4-Edit or update tasks\n5-help\n6-EXIT\n");
+    set_color(15);
 }
 
 void DISPLAY_TIME_DATE(struct tm t)
@@ -97,8 +106,10 @@ void ADD_TASK(int *count, string *name, string *description, LIST_TASK *list)
     }
     else
     {
-
+        set_color(12);
         puts("no space\n");
+        set_color(15);
+
     }
 }
 
@@ -107,7 +118,7 @@ void DISPLAY_ALL_TASKS(LIST_TASK *list, int numberoftasks)
 
     for (int i = 0; i < numberoftasks; i++)
     {
-
+        
         printf("\n%d- %s\n ", i + 1, (*list)[i].NAME);
         PRINT_INFO(*list[i]);
     }
@@ -122,9 +133,10 @@ void loading()
     {
 
         increment = (rand() % 12) + 2;   // randomising the incremntaion
-        sleeper = (rand() % 1000) + 900; // daki kif kif akken ad iban ad yatel chuya
+        sleeper = (rand() % 500) + 200; // daki kif kif akken ad iban ad yatel chuya
 
         start = start + increment;
+        set_color(1);
         printf("\r[###### - %d%% -######]", start);
         Sleep(sleeper);
 
@@ -133,8 +145,11 @@ void loading()
 
 void help()
 {
-    printf("THIS IS THE PRIVATE PROPERTY OF MEHDI CHERBI\n PROTCTED BY COPYRIGHT \n ANY COMMENTS OR COMPLAINTS MUST BE DIRECTED TO HIM\n THIS IS A SPAGHETTI_CODE VERSION OF A TASK MANAGER. I AIN'T CLAIMING IT'S PERFECT BUT IT'S A STEP MORE TO LEARN NEW CONCEPTS. DATA STRUCT RIP");
+
+    printf(" THIS IS THE PRIVATE PROPERTY OF MEHDI CHERBI\n PROTCTED BY COPYRIGHT \n ANY COMMENTS OR COMPLAINTS MUST BE DIRECTED TO HIM\n THIS IS A SPAGHETTI_CODE VERSION OF A TASK MANAGER. I AIN'T CLAIMING IT'S PERFECT BUT IT'S A STEP MORE TO LEARN NEW CONCEPTS. DATA STRUCT RIP");
+    set_color(12);
     puts("\n\npress esc to go back");
+    set_color(15);
     while (1)
     {
         if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
